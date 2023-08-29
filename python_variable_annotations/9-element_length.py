46507edb63aed4f6a3ba753829fd856d2d7579e6
alemao51092 <aarielmartinez1111@gmail.com>
2023-08-29 08:05:00 -0700

    task9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""shebang"""
+
+from typing import List, Tuple, Iterable, Sequence, Union
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Union[Sequence, int]]]:  # Noqa: E501
+    """element"""
+    return [(i, len(i)) for i in lst]
