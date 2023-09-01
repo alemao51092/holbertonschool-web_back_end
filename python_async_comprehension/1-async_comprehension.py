af3881ce8aee90559767749cd3bb412d25f1f969
alemao51092 <aarielmartinez1111@gmail.com>
2023-09-01 09:54:04 -0700

    task1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """botellas caras"""
     async_list = [i async for i in async_generator()]
     return async_list
