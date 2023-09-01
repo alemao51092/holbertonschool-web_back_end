9639fceeeccec30a67fb1c673bcef89da435b3f4
alemao51092 <aarielmartinez1111@gmail.com>
2023-09-01 09:52:37 -0700

    task1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """shebang"""
 
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     async_list = [i async for i in async_generator()]
     return async_list
