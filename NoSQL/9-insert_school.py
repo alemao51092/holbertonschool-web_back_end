075d41de8691e7c9ee7ac855829b105331de6843
alemao51092 <aarielmartinez1111@gmail.com>
2023-09-08 10:47:36 -0700

    task9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 9-main """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection
+    based on kwargs"""
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
